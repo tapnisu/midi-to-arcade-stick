16e0323169ae20f3a58bae10e4d2eea0dc56e58f
Aleksei Rybin <aleksei@tapni.su>
2026-03-05 15:44:26 +0700

    feat: implement stick input

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,74 @@
+use midir::{MidiInput, MidiInputConnection};
+use midly::live::LiveEvent;
+use std::error::Error;
+use std::io::{Write, stdin, stdout};
+
 use std::thread;
 use std::time::Duration;
 use vigem_client::TargetId;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
+    let midi_in = MidiInput::new("midi_listener")?;
+    let in_ports = midi_in.ports();
+
     let client = vigem_client::Client::connect().unwrap();
     let mut target = vigem_client::Xbox360Wired::new(client, TargetId::XBOX360_WIRED);
 
     target.plugin().unwrap();
-
     target.wait_ready().unwrap();
 
-    let gamepad_pressed = vigem_client::XGamepad {
+    if in_ports.is_empty() {
+        unimplemented!()
+    }
+
+    let port = &in_ports[0];
+    let port_name = midi_in.port_name(port)?;
+
+    let mut gamepad_pressed = vigem_client::XGamepad {
         buttons: vigem_client::XButtons!(A),
-        thumb_lx: 256,
         ..Default::default()
     };
 
-    let gamepad_empty = vigem_client::XGamepad::default();
+    let _conn = midi_in.connect(
+        port,
+        &port_name,
+        move |_timestamp, data, _| {
+            if let Ok(event) = LiveEvent::parse(data) {
+                match event {
+                    LiveEvent::Midi {
+                        channel: _,
+                        message,
+                    } => match message {
+                        midly::MidiMessage::NoteOn { key, vel } => {
+                            println!("Key Pressed:  {} (Velocity: {})", key, vel);
+                        }
+                        midly::MidiMessage::NoteOff { key, .. } => {
+                            println!("Key Released: {}", key);
+                        }
+                        midly::MidiMessage::PitchBend { bend } => {
+                            gamepad_pressed.thumb_lx = bend.as_int() * 4;
+                            let _ = target.update(&gamepad_pressed);
+                        }
+                        midly::MidiMessage::Controller { controller, value } => {
+                            if controller.as_int() == 1 {
+                                gamepad_pressed.thumb_ly = value.as_int() as i16 * -256 + -1;
+                                let _ = target.update(&gamepad_pressed);
+                            } else {
+                                println!("Control #{} value: {}", controller, value);
+                            }
+                        }
+                        _ => {}
+                    },
+                    _ => {}
+                }
+            }
+        },
+        (),
+    )?;
 
-    loop {
-        let _ = target.update(&gamepad_pressed);
-        thread::sleep(Duration::from_millis(500));
+    println!("Press Enter to stop...");
+    let mut input = String::new();
+    stdin().read_line(&mut input)?;
 
-        let _ = target.update(&gamepad_empty);
-        thread::sleep(Duration::from_millis(500));
-    }
+    Ok(())
 }
